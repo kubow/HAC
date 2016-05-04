7cd27b387dee0a1335c02de9d0d66e037dde534e
Kube Kubow <kubow@users.noreply.github.com>
2016-05-04 21:43:26 +0200

    Added files via upload

---
+++ jidelak.bat
@@ -0,0 +1,17 @@
+echo on
+Set index=%~dp0iisstart.htm
+wscript %~dp0Reader\Reader_Menu.vbs
+echo ^<html^> > %index%
+echo ^<head^> >> %index%
+echo ^<meta http-equiv="Content-Type" content="text/html; charset=windows-1250" /^> >> %index%
+echo ^<title^>Jidelni_listek^</title^> >> %index%
+echo ^<link rel="stylesheet" type="text/css" href="Reader/style.css"^> >> %index%
+echo ^</head^> >> %index%
+echo ^<body^> >> %index%
+echo ^<div id="frame"^>^<iframe src="Reader/jidelak/jidlo1.htm" width="100%%" height="100%%"^>^</iframe^>^</div^> >> %index%
+echo ^<div id="top"^>^<marquee behavior="scroll" direction="left" scrolldelay="100"^> >> %index%
+rem echo ^<div id="top"^>^<marquee behavior="scroll" direction="left" scrolldelay="100" scrollamount="3"^> >> %index%
+type %~dp0Presenter\text.txt >> %index%
+echo ^</marquee^>^</div^> >> %index%
+echo ^</body^> >> %index%
+echo ^</html^> >> %index%
---
+++ prezentace.bat
@@ -0,0 +1,26 @@
+echo on
+Set index=%~dp0iisstart.htm
+Set HTMLChange=%~dp0Presenter\index.html
+echo ^<html^> > %index%
+echo ^<head^> >> %index%
+echo ^<meta http-equiv="Content-Type" content="text/html; charset=windows-1250" /^> >> %index%
+echo ^<title^>Prezentace^</title^> >> %index%
+echo ^<link rel="stylesheet" type="text/css" href="Reader/style.css"^> >> %index%
+echo ^</head^> >> %index%
+echo ^<body^> >> %index%
+echo ^<div id="frame"^>^<iframe src="Presenter/index.html" width="100%%" height="100%%"^>^</iframe^>^</div^> >> %index%
+echo ^<div id="top"^>^<marquee behavior="scroll" direction="left" scrolldelay="100"^> >> %index%
+rem echo ^<div id="top"^>^<marquee behavior="scroll" direction="left" scrolldelay="100" scrollamount="3"^> >> %index%
+type  %~dp0Presenter\text.txt >> %index%
+echo ^</marquee^>^</div^> >> %index%
+echo ^</body^> >> %index%
+echo ^</html^> >> %index%
+rem http://stackoverflow.com/questions/5642021/batch-process-all-files-in-directory
+type %~dp0Presenter\HTML_head.txt > %HTMLChange%
+for %%F in (%~dp0Presenter\image\*.jpg) do (
+   echo ^<li^> >> %HTMLChange%
+   echo ^<span class="Centerer"^>^</span^> >> %HTMLChange%
+   echo ^<img class="Centered" src="image\%%~nxF" alt="X"/^> >> %HTMLChange%
+   echo ^</li^> >> %HTMLChange%   
+)
+type %~dp0Presenter\HTML_tail.txt >> %HTMLChange%
