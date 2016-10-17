2ce5e3b2ca9ea6ea43c5c4ec9791b684afdaaf60
kubow <kube.kubow@gmail.com>
2016-10-17 07:29:43 +0200

    HTML reader update

--- Reader/HTML.py
+++ Reader/HTML.py
@@ -1,4 +1,13 @@
-import HTMLParser
+import os
+import sqlite3
+import requests
+try:
+  from bs4 import BeautifulSoup
+  import HTMLParser
+  easier = True
+except:
+  import HTMLParser
+  easier = False
 
 # http://stackoverflow.com/questions/3276040/how-can-i-use-the-python-htmlparser-library-to-extract-data-from-a-specific-div
 
@@ -8,14 +17,14 @@ class LinksParser(HTMLParser.HTMLParser):
     self.recording = 0
     self.data = []
 
-  def handle_starttag(self, tag, attributes):
+  def handle_starttag(self, tag, attributes): #, tag_type, tag_name):
     if tag != 'div':
       return
     if self.recording:
       self.recording += 1
       return
     for name, value in attributes:
-      if name == 'id' and value == 'remository':
+      if name == 'id' and value == 'contentwsw':
         break
     else:
       return
@@ -29,8 +38,35 @@ class LinksParser(HTMLParser.HTMLParser):
     if self.recording:
       self.data.append(data)
 
-p = LinksParser()
-f = urllib.urlopen("http://domain.com/somepage.html")
-html = f.read()
-p.feed(html)
-p.close()
+def process_url(url, tag_name):
+  '''proccess text from url, given url, tag type and id'''
+  html = requests.get(url)
+  if easier:
+    soup = BeautifulSoup(html.content, "lxml") 
+    div = soup.find('div', id=tag_name)
+    return ''.join(map(str, div.contents))
+  else:
+    p = LinksParser()
+    div = p.feed(html)
+    p.close()
+    return div
+
+def write_html(file_name, content):
+  '''write html content to a file using template'''
+  print file_name
+  print content
+  
+''' read restaurant menus, write to separate htm files'''
+conn=sqlite3.connect(os.getcwd()+'/Reader.db')
+rest=conn.execute('SELECT * FROM RestActive;').fetchall()
+for row in rest:
+  print row[3]+' > '+row[2]+'.htm'
+  if not row[4]:
+    # zomato address
+    parsed_content=process_url(row[5], 'daily-menu-container')
+  else:
+    # dependalble on input text
+    parsed_content=process_url(row[4], row[6])
+  write_html(os.getcwd()+'/'+row[2]+'.htm', )
+
+
