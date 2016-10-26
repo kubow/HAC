69317c977129970f25869c88c72ae52e4344fe31
Kube Kubow <kubow@tiscali.cz>
2016-10-26 17:12:07 +0200

    packages list to install

--- Connector/installer.bat
+++ Connector/installer.bat
@@ -1,3 +1,13 @@
-REM check if python path in variables
+@echo off
+REM check if python path in variable PATH (presume python installed)
+REM for /f "delims=" %%a in ('REG QUERY "HKLM\SOFTWARE\Python\PythonCore" /s ^| findstr InstallPath') do  (
+REM   set key=%%a
+REM   goto :endfor
+REM )
+REM :endfor
+REM for /f "tokens=2*" %%a in ('REG QUERY %key% /ve') do set "CHESSPYTHONPATHv1=%%~bpython.exe"
+REM echo %CHESSPYTHONPATHv1%
 REM check if pip path in variables
-python installer.py PySide pyqtgraph
+SET /p args=<%~dp0py_packages.txt
+echo checking if installed: %args%
+python installer.py %args%
