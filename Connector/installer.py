a8e838dee73113ccd1ea5ffe60fe0a0d15eb79d8
Kube Kubow <kubow@tiscali.cz>
2016-10-26 17:11:18 +0200

    installer updated

--- Connector/installer.py
+++ Connector/installer.py
@@ -1,19 +1,34 @@
+'''simple function to install missing packages
+using command line arguments as package names
+then trying to import or install them with pip'''
 import sys
 import importlib
 import pip
 
 def install(package):
-    pip.main(['install', package])
-
-for arg in sys.argv[1:]:
+    '''actual installation'''
     try:
-        print arg
-#        map(__import__, arg)
-        importlib.import_module(arg)
-        print 'succesfully imported ' + arg
-    except ImportError:
-        print arg + ' is not installed, installing it now!'
-        install(arg)
+        pip.main(['install', package])
+        importlib.import_module(package)
+        print 'successfully installed "' + package + '"'
+    except:
+        print 'error installing "' + package + '"'
+        global all_good
+        all_good = False
 
-print 'all of packages should be fine ...'
+'''parse command line arguments'''
+all_good = True
+for pkg in sys.argv[1:]:
+    try:
+        print pkg
+#        map(__import__, pkg)
+        importlib.import_module(pkg)
+        print 'package "' + pkg + '" is installed'
+    except ImportError:
+        print 'package "' + pkg + '" is not installed, installing now...'
+        install(pkg)
+if all_good:
+    print 'all of packages should be fine ...'
+else:
+    print 'there was some error...'
 sys.exit(1)
