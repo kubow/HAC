e3a4db10faa13776d25740009e8ab6f92eea5e79
Kube Kubow <kubow@tiscali.cz>
2016-12-17 21:40:43 +0100

    prezentace win / linux repaired

--- prezentace.bat
+++ prezentace.bat
@@ -1,26 +1,34 @@
-echo on
-Set index=%~dp0iisstart.htm
-Set HTMLChange=%~dp0Multimedia\presenting.html
-echo ^<html^> > %index%
-echo ^<head^> >> %index%
-echo ^<meta http-equiv="Content-Type" content="text/html; charset=windows-1250" /^> >> %index%
-echo ^<title^>Prezentace^</title^> >> %index%
-echo ^<link rel="stylesheet" type="text/css" href="Reader/style.css"^> >> %index%
-echo ^</head^> >> %index%
-echo ^<body^> >> %index%
-echo ^<div id="frame"^>^<iframe src="Presenter/index.html" width="100%%" height="100%%"^>^</iframe^>^</div^> >> %index%
-echo ^<div id="top"^>^<marquee behavior="scroll" direction="left" scrolldelay="100"^> >> %index%
-rem echo ^<div id="top"^>^<marquee behavior="scroll" direction="left" scrolldelay="100" scrollamount="3"^> >> %index%
-type  %~dp0Multimedia\text_horni_lista.txt >> %index%
-echo ^</marquee^>^</div^> >> %index%
-echo ^</body^> >> %index%
-echo ^</html^> >> %index%
+@echo off
+
+Set presName=Multimedia
+Set tempName=Structure
+Set presDir=%~dp0%presName%\
+Set tempDir=%~dp0%tempName%\
+Set mainHTML=%~dp0index.html
+Set subHTML=%presDir%presenting.html
+
+echo ^<html^> > %mainHTML%
+echo ^<head^> >> %mainHTML%
+echo ^<meta http-equiv="Content-Type" content="text/html; charset=windows-1250" /^> >> %mainHTML%
+echo ^<title^>Prezentace^</title^> >> %mainHTML%
+echo ^<link rel="stylesheet" type="text/css" href="%tempName%/style.css"^> >> %mainHTML%
+echo ^</head^> >> %mainHTML%
+echo ^<body^> >> %mainHTML%
+echo ^<div id="frame"^>^<iframe src="%presName%/presenting.html" width="100%%" height="100%%"^>^</iframe^>^</div^> >> %mainHTML%
+echo ^<div id="top"^>^<marquee behavior="scroll" direction="left" scrolldelay="100"^> >> %mainHTML%
+rem echo ^<div id="top"^>^<marquee behavior="scroll" direction="left" scrolldelay="100" scrollamount="3"^> >> %mainHTML%
+type  %presDir%text_horni_lista.txt >> %mainHTML%
+echo ^</marquee^>^</div^> >> %mainHTML%
+echo ^</body^> >> %mainHTML%
+echo ^</html^> >> %mainHTML%
 rem http://stackoverflow.com/questions/5642021/batch-process-all-files-in-directory
-type %~dp0Presenter\HTML_head.txt > %HTMLChange%
-for %%F in (%~dp0\Multimedia\image\*.jpg) do (
-   echo ^<li^> >> %HTMLChange%
-   echo ^<span class="Centerer"^>^</span^> >> %HTMLChange%
-   echo ^<img class="Centered" src="image\%%~nxF" alt="X"/^> >> %HTMLChange%
-   echo ^</li^> >> %HTMLChange%   
+echo Presenting images ...
+type %tempDir%HTML_Presenter_head.txt > %subHTML%
+for %%F in (%presDir%image\*.jpg) do (
+    echo %presDir%image\%%~nxF
+    echo ^<li^> >> %subHTML%
+    echo ^<span class="Centerer"^>^</span^> >> %subHTML%
+    echo ^<img class="Centered" src="image\%%~nxF" alt="X"/^> >> %subHTML%
+    echo ^</li^> >> %subHTML%   
 )
-type %~dp0Presenter\HTML_tail.txt >> %HTMLChange%
+type %tempDir%HTML_Presenter_tail.txt >> %subHTML%
--- prezentace.sh
+++ prezentace.sh
@@ -1,34 +1,37 @@
 #!/bin/sh
 
+presName=Multimedia
+tempName=Structure
+presDir=${PWD}'/'${presName}
+tempDir=${PWD}'/'${tempName}
 mainHTML=${PWD}'/index.html'
-subHTML=${PWD}'/Presenter/index.html'
+subHTML=${PresDir}'/index.html'
 
 echo '<html>' > $mainHTML
 echo '<head>' >> $mainHTML
-echo '	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' >> $mainHTML
-echo '	<title>Prezentace</title>' >> $mainHTML
-echo '	<link rel="stylesheet" type="text/css" href="Reader/style.css">' >> $mainHTML
+echo '  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />' >> $mainHTML
+echo '  <title>Prezentace</title>' >> $mainHTML
+echo '  <link rel="stylesheet" type="text/css" href="'${tempName}'/style.css">' >> $mainHTML
 echo '</head>' >> $mainHTML
 echo '<body>' >> $mainHTML
-
-echo '<div id="frame"><iframe src="Presenter/index.html" width="100%%" height="100%%"></iframe></div>' >> $mainHTML
+echo '<div id="frame"><iframe src="'${presName}'/index.html" width="100%%" height="100%%"></iframe></div>' >> $mainHTML
 echo '<div id="top"><marquee behavior="scroll" direction="left" scrolldelay="100">' >> $mainHTML
 #echo '<div id="top"><marquee behavior="scroll" direction="left" scrolldelay="100" scrollamount="3">' >> $mainHTML
-cat  ${PWD}'/Presenter/text.txt' >> $mainHTML
+cat  ${PresDir}'/text.txt' >> $mainHTML
 echo '</marquee></div>' >> $mainHTML
 echo '</body>' >> $mainHTML
 echo '</html>' >> $mainHTML
 
-# echo $subHTML
+# echo 'Presenting images ...'
 
-cat ${PWD}'/Presenter/HTML_head.txt' > $subHTML
-for f in ${PWD}/Presenter/image/*
+cat ${tempDir}'/HTML_Presenter_head.txt' > $subHTML
+for f in ${PresDir}'/image/*'
 do
-	echo '<li>' >> $subHTML
-	echo '<span class="Centerer"></span>' >> $subHTML
-	echo ${f##*/}
-	echo '<img class="Centered" src="image\' >> $subHTML
-	echo ${f##*/}'" alt="X"/>' >> $subHTML
-	echo '</li>' >> $subHTML
+    echo '<li>' >> $subHTML
+    echo '<span class="Centerer"></span>' >> $subHTML
+    echo ${PresDir}'/image/'${f##*/}
+    echo '<img class="Centered" src="image\' >> $subHTML
+    echo ${f##*/}'" alt="X"/>' >> $subHTML
+    echo '</li>' >> $subHTML
 done
-cat ${PWD}'/Presenter/HTML_tail.txt' >> $subHTML
+cat ${tempDir}'/HTML_Presenter_tail.txt' >> $subHTML
