0d8ea38f94aea64ddf180a7c30319692df28095d
kubow <kube.kubow@gmail.com>
2017-01-15 23:50:40 +0100

    weather modyfied

--- System/DataWeather/WriteWeatherActual.py
+++ System/DataWeather/WriteWeatherActual.py
@@ -22,24 +22,22 @@ owm = pyowm.OWM('1050e850fbcc463dd98a726d6af37134')  # You MUST provide a valid 
 
 obs = owm.weather_at_place(loc)
 w = obs.get_weather()
+# Weather details
+# w.get_wind()                  # {'speed': 4.6, 'deg': 330}
+#w.get_humidity()              # 87
+#w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
 
 htm=open(write_file_path, 'w+')
 print 'file '+write_file_path+'/weather.htm created...'
 htm.write('<HTML>\n<HEAD>\n<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">\n')
 htm.write('<TITLE>Weather at '+loc+'</TITLE>\n</HEAD>\n<BODY>\n<H1>')
 htm.write('Weather at '+loc+'</H1>\n<P>')
+#htm.write(w = str(obs.get_weather()))
 htm.write('Nearest station: '+obs._location._name+' ('+obs._location._country+')\n</BR>')
-htm.write(w.XML())
-#htm.write(w.get_temperature('celsius'))
-#htm.write(w.get_humidity())
-#htm.write(w.get_wind())
+htm.write('Temperature: '+str(w.get_temperature('celsius'))+'\n</BR>')
+htm.write('Wind: '+str(w.get_wind())+'\n</BR>')
+htm.write('Humidity: '+str(w.get_humidity())+'\n</BR></P>')
 htm.write('</BODY>\n</HTML>')
 
-
-#print(w)                      # <Weather - reference time=2013-12-18 09:20,status=Clouds>
-
-# Weather details
-w.get_wind()                  # {'speed': 4.6, 'deg': 330}
-w.get_humidity()              # 87
-w.get_temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+htm.close
 
--- pocasi.sh
+++ pocasi.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #!/usr/bin python
 
+#location='Horni Pocernice,cz'
+location='Praha,cz'
 mainHTML=${PWD}'/index.html'
 pythonFILE=${PWD}/System/DataWeather/WriteWeatherActual.py
 
 #python generate htm's
 echo ${pythonFILE}
-python ${pythonFILE} ${mainHTML} 'Horni Pocernice,cz'
+#python ${pythonFILE} ${mainHTML} 
+python ${pythonFILE} ${mainHTML} ${location}
 
-cat ${PWD}'/Structure/HTML_Base_tail.txt' >> $mainHTML
+#cat ${PWD}'/Structure/HTML_Base_tail.txt' >> $mainHTML
