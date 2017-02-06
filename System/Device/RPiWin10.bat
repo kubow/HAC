96f873de4d0e3e79516f2faa3a988be0ab1043bc
kubow <kube.kubow@gmail.com>
2017-02-06 16:00:08 +0100

    RPi on Widows10 player add

---
+++ System/Device/RPiWin10.bat
@@ -0,0 +1,5 @@
+echo off
+cls
+powershell.exe -ExecutionPolicy Bypass -Command "c:\_Run\Script\PSplay\play.ps1"
+
+pause
---
+++ System/Device/RPiWin10.ps1
@@ -0,0 +1,34 @@
+﻿<# Used to connect inside Raspberry Pi #>
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value RPi2Win10Dev
+<# IP: 172.20.31.138 Password: Aaa123456bcd789 #>
+<# Enter-PSSession -ComputerName RPi2Win10Dev -Credential RPi2Win10Dev\Administrator #>
+Enter-PSSession -ComputerName RPi2Win10Dev -Credential RPi2Win10Dev\Administrator
+<# several basic commands #> 
+tlist
+Get-Service
+IoTStartup list
+<# Registry HKEY_CURRENT_USER #>
+cd hkcu:
+<# Cycle services - stop, restart ... #>
+Restart-Service DHCP
+<# Prepare things on MiniPC #>
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+Import-Module NetTCPIP
+Get-NetTCPSetting
+Get-NetRoute
+Get-NetUDPEndpoint
+
+
+
+schtasks /run /tn StartMsvsmon
+
+Startup /d
+
+<# Used for restart and stop #>
+Stop-Computer -WsmanAuthentication Basic -computer 172.20.31.113 -Credential 172.20.31.113\Administrator
+Restart-computer -WsmanAuthentication Basic
+<# installing other things #>
+$env:DNX_UNSTABLE_FEED = "https://www.myget.org/F/aspnetmaster"
+dnvm upgrade
+dnvm update-self
+
