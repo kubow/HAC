41f7e919d1ad5dd7acb5d706d9379a20ef94c73f
kubow <kube.kubow@gmail.com>
2017-02-17 00:35:10 +0100

    pocasi sckript update

--- pocasi.sh
+++ pocasi.sh
@@ -9,18 +9,24 @@ settings_db=${PWD}/System/Device/settings.db
 py_dev_file=${PWD}/System/Device/DataWrite.py
 py_for_file=${PWD}/System/DataWeather/WriteWeatherActual.py
 
-#location='city,country'
-IFS='|' read -r loc cnt <<< $(sqlite3 ${settings_db} 'select * from place_active')
+echo ============================================
+echo "getting location in format ('city','country')"
+echo ============================================
+command=$(sqlite3 ${settings_db} 'select * from place_active')
+IFS='|' read -r loc cnt <<< ${command}
 location=${loc},${cnt}
-
-#python read forecast
-#====================
+echo got: ${location} : 'select * from place_active'
+echo ====================
+echo python read forecast 
+echo ====================
+echo syntax: ${py_for_file} file_to_write location (now disabled)
 #syntax: py_for_file file_to_write location
 #forecast temporarily diabled
 #python ${py_for_file} ${mainHTML} ${location}
 
-#python read actual weather data
-#===============================
+echo ==========================
+echo python read weather actual
+echo ==========================
 weather_db=${PWD}/Multimedia/Weather/${loc}_${YY}${MM}.db
 platform=$(sqlite3 ${settings_db} 'select devicename from device where active=1')
 echo ${platform} - writing ${loc}_${YY}${MM}.db
