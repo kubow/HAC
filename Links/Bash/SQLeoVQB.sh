92a96411d718d933dbe456f9f8893d9ad540ca63
kubow <kube.kubow@gmail.com>
2017-04-18 08:05:57 +0200

    shortcuts for SQLeo created

---
+++ Links/Bash/SQLeoVQB.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+jar_file=/home/kubow/Dokumenty/App/SQLeoVQB.2016.05.rc1/SQLeoVQB.jar
+#java -jar ${jar_file} "$@"
+java -jar ${jar_file}
--- Links/Bash/jpass.sh
+++ Links/Bash/jpass.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cd /home/kubow/Dokumenty/App/jpass
-#java -jar jpass-0.1.14.jar "$@"
-java -jar jpass-0.1.14.jar /home/kubow/Dropbox/BrainMemory.jpass
+jar_file=/home/kubow/Dokumenty/App/jpass/jpass-0.1.14.jar
+#java -jar ${jar_file} "$@"
+java -jar ${jar_file} /home/kubow/Dropbox/BrainMemory.jpass
