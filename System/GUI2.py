a659b1f995026a1188a4a5fb7ea06419bc6c9190
Kube Kubow <kubow@tiscali.cz>
2017-05-23 16:30:11 +0200

    GUI play

--- System/GUI2.py
+++ System/GUI2.py
@@ -1,32 +1,35 @@
 #https://www.youtube.com/watch?v=eL_sy9TqCBE
 #http://www.sharpertradingimage.com/python-listbox-delete-text/
 from Tkinter import *
-impoert os
+import os
 
 def get_image():
     mlt_img['image'] = mlt_lib[lb.get('active')]
 
 def get_mlt_lib(directory):
-    mlt_lib = {}
-    for mlt_file in os.listdir(directory):
-        # for now just images
-        if not '.gif' in mlt_file:
-            continue
-        mlt_lib[mlt_file]
-
-root = Tk()
-
-mlt_lib = {
+    """return multimedia library in format:
+    mlt_lib = {
     'new1': PhotoImage(file='/home/kubow/Dropbox/Web/64/Astrologie/Planet2.gif'),
     'new2': PhotoImage(file='/home/kubow/Dropbox/Web/64/Astrologie/tiamatsplit.gif'),
     'win1': PhotoImage(file='/home/kubow/Dropbox/Web/64/Astrologie/Nibiru-Smites-2.gif')
     #'win1': PhotoImage(file='c:\\Users\\JAV\\Dropbox\\Web\\64\\Astrologie\\Planet2.gif')
     }
+    """
+    mlt_lib = {}
+    for mlt_file in os.listdir(directory):
+        # for now just images
+        if not '.gif' in mlt_file:
+            continue
+        mlt_lib[mlt_file] = PhotoImage(file=directory+mlt_file)
+    return mlt_lib
+
+root = Tk()
 
 #img = PhotoImage(file=filename)
+mlt_lib = get_mlt_lib('c:\\Users\\JAV\\Dropbox\\Web\\64\\Astrologie\\')
 
 txtng = 'showing picture in list'
-mlt_img = Label(root, image=mlt_lib['new1'], text='sadasdsadassdds')
+mlt_img = Label(root, image=mlt_lib[next(iter(mlt_lib))], text='sadasdsadassdds')
 button = Button(root, text=txtng, command=get_image)
 
 # listbox
