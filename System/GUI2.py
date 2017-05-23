5fdd2c7f687d799aa53a0ae459ab15a78a92b6a9
kubow <kube.kubow@gmail.com>
2017-05-24 01:03:03 +0200

    linux gui tweak

--- System/GUI2.py
+++ System/GUI2.py
@@ -2,18 +2,14 @@
 #http://www.sharpertradingimage.com/python-listbox-delete-text/
 from Tkinter import *
 import os
+import argparse
 
 def get_image():
     mlt_img['image'] = mlt_lib[lb.get('active')]
 
 def get_mlt_lib(directory):
     """return multimedia library in format:
-    mlt_lib = {
-    'new1': PhotoImage(file='/home/kubow/Dropbox/Web/64/Astrologie/Planet2.gif'),
-    'new2': PhotoImage(file='/home/kubow/Dropbox/Web/64/Astrologie/tiamatsplit.gif'),
-    'win1': PhotoImage(file='/home/kubow/Dropbox/Web/64/Astrologie/Nibiru-Smites-2.gif')
-    #'win1': PhotoImage(file='c:\\Users\\JAV\\Dropbox\\Web\\64\\Astrologie\\Planet2.gif')
-    }
+    mlt_lib = {'filename': PhotoImage(file='*.gif')}
     """
     mlt_lib = {}
     for mlt_file in os.listdir(directory):
@@ -23,27 +19,49 @@ def get_mlt_lib(directory):
         mlt_lib[mlt_file] = PhotoImage(file=directory+mlt_file)
     return mlt_lib
 
-root = Tk()
+def onselect(evt):
+    w = evt.widget
+    print w.curselection()
+    index = int(w.curselection()[0])
+    value = w.get(index)
+    print 'You selected item %d: "%s"' % (index, value)
+    mlt_img['image'] = mlt_lib[value]
 
-#img = PhotoImage(file=filename)
-mlt_lib = get_mlt_lib('c:\\Users\\JAV\\Dropbox\\Web\\64\\Astrologie\\')
+def multimedia_list():
+    # list of multimedia files (gif images)
+    lb = Listbox(root, height=7)
+    lb.bind('<<ListboxSelect>>', onselect)
+    for img in mlt_lib.keys():
+        lb.insert('end', img)
+    
+    yscroll = Scrollbar(root, orient=VERTICAL)
+    lb['yscrollcommand'] = yscroll.set
+    yscroll['command'] = lb.yview
 
-txtng = 'showing picture in list'
-mlt_img = Label(root, image=mlt_lib[next(iter(mlt_lib))], text='sadasdsadassdds')
-button = Button(root, text=txtng, command=get_image)
+    mlt_img.grid(row=0, column=0, sticky="wn")
+    
+    lb.grid(row=0, column=1, rowspan=2, sticky=N+S)
+    yscroll.grid(row=0, column=1, rowspan=2, sticky=N+S+E)
+    
 
-# listbox
-lb = Listbox(root, height=7)
-for img in mlt_lib.keys():
-    lb.insert('end', img)
-
-yscroll = Scrollbar(root, orient=VERTICAL)
-lb['yscrollcommand'] = yscroll.set
-yscroll['command'] = lb.yview
-
-mlt_img.grid(row=0, column=0, sticky="wn")
-button.grid(row=1, column=0)
-lb.grid(row=0, column=1, rowspan=2, sticky=N+S)
-yscroll.grid(row=0, column=1, rowspan=2, sticky=N+S+E)
-
-root.mainloop()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="run over dir")
+    parser.add_argument('-d', help='directory', type=str, default='')
+    args = parser.parse_args()
+    # temporarily run over one dir, will be browser further
+    #implement max image size
+    root = Tk()
+    #img = PhotoImage(file=filename)
+    if os.path.isdir('c:\\Users\\JAV\\Dropbox\\Web\\64\\Astrologie\\'):
+        mlt_lib = get_mlt_lib('c:\\Users\\JAV\\Dropbox\\Web\\64\\Astrologie\\')
+    else:
+        mlt_lib = get_mlt_lib('/home/kubow/Dropbox/Web/64/Astrologie/')
+        
+    txtng = 'showing picture in list'
+    mlt_img = Label(root, image=mlt_lib[next(iter(mlt_lib))])
+    #button = Button(root, text=txtng, command=get_image)
+    #button.grid(row=1, column=0)
+    bar = Menu(root, background='red', relief='flat')
+    multimedia_list()
+    
+    root.mainloop()
--- h808e.sh
+++ h808e.sh
@@ -3,47 +3,47 @@
 # decide if dialog installed (debian, ubuntu)
 runflag=1
 # Loop forever (until break is issued)
-while true; do
-	#if [ "$runflag" -eq "0" ]; then
-	#	echo "must quit..."
-	#else
-		#clear the screen
-		clear
-		#show the menu
-		echo "============= -H_808_E- ============="
-		echo "-------------------------------------"
-		echo "1.  Open encyklopedia cherrytree"
-		echo "2.  Open encyklopedia sqlite browser"
-		echo "3.  Directory synchronizer"
-		echo "4.  Generate structure from db"
-		echo "5.  A"
-		echo "6.  B"
-		echo "7.  C"
-		echo "-------------------------------------"
-		echo "8.  Universal python project"
-		echo "-------------------------------------"
-		echo "9.  Browse pages in (FF/CH/IE)"
-		echo "-------------------------------------"
-		echo "==========PRESS 'Q' TO QUIT=========="
+python /home/kubow/Dokumenty/Script/System/GUI2.py -d /home/kubow/Dokumenty/Web/64
+#while true; do
+    #if [ "$runflag" -eq "0" ]; then
+    #    echo "must quit..."
+    #else
+        #clear #clear the screen
+        #show the menu
+        #echo "============= -H_808_E- ============="
+        #echo "-------------------------------------"
+        #echo "1.  Open encyklopedia cherrytree"
+        #echo "2.  Open encyklopedia sqlite browser"
+        #echo "3.  Directory synchronizer"
+        #echo "4.  Generate structure from db"
+        #echo "5.  A"
+        #echo "6.  B"
+        #echo "7.  C"
+        #echo "-------------------------------------"
+        #echo "8.  Universal python project"
+        #echo "-------------------------------------"
+        #echo "9.  Browse pages in (FF/CH/IE)"
+        #echo "-------------------------------------"
+        #echo "==========PRESS 'Q' TO QUIT=========="
 
-		options=("1" "2" "3" "4" "5" "6" "7" "8" "9" "0")
-		PS3="Please select a number:"
-		select opt in "${options[@]}"
-		do 
-			case $opt in
-				"1" ) cherrytree;;
-				"2" ) sqlitebrowser;;
-				"3" ) echo "for all files in directory: /home/kubow/Dokumenty/Web";;
-				"4" ) python /home/kubow/Dokumenty/H808E_gen.py;;
-				"5" ) echo "Y";;
-				"6" ) echo "You $opt";;
-				"7" ) echo "Youked $opt";;
-				"8" ) echo "You pic $opt";;
-				"9" ) echo "You pcd $opt";;
-				"0" ) echo "You picked $opt which is option $REPLY";;
-				*) echo invalid option
-			esac
-		done
-	#fi
+        #options=("1" "2" "3" "4" "5" "6" "7" "8" "9" "0")
+        #PS3="Please select a number:"
+        #select opt in "${options[@]}"
+        #do 
+            #case $opt in
+                #"1" ) cherrytree;;
+                #"2" ) sqlitebrowser;;
+                #"3" ) echo "for all files in directory: /home/kubow/Dokumenty/Web";;
+                #"4" ) python /home/kubow/Dokumenty/H808E_gen.py;;
+                #"5" ) echo "Y";;
+                #"6" ) echo "You $opt";;
+                #"7" ) echo "Youked $opt";;
+                #"8" ) echo "You pic $opt";;
+                #"9" ) echo "You pcd $opt";;
+                #"0" ) echo "You picked $opt which is option $REPLY";;
+                #*) echo invalid option
+            #esac
+        #done
+    #fi
 done
 
