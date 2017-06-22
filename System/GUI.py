b61c814c28fa70ecaa6f65e8d7b108549547e671
Kube Kubow <kubow@tiscali.cz>
2017-06-22 16:49:55 +0200

    improved GUI structure

--- System/GUI.py
+++ System/GUI.py
@@ -1,66 +1,114 @@
 # -*- coding: utf-8 -*-
 import os
 import argparse
+# Use Tkinter for python 2, tkinter for python 3
 from Tkinter import *
 from PIL import Image, ImageTk
 # local project imports
 import H808E
-import ProcessText
+import TextProcess
+
+
+class MainWindow:
+    def __init__(self, master):
+        self.master = master
+        self.frame = Frame(self.master)
+
+        # upper menu
+        self.bar_main = Label(self.master, text=' Main Menu ...')
+        self.bar_node = Label(self.master, text='Location : {0}'.format('C:\\'))
+        self.bar_main.grid(row=0, column=0, sticky=W)
+        self.bar_node.grid(row=0, column=1, sticky=E)
+
+        # multimedia view window
+        self.canvas = Canvas(self.master, bd=0, highlightthickness=0, width=600, height=600, background="white")
+        self.canvas.grid(row=1, column=0, rowspan=2, columnspan=2, sticky=N + S + W, pady=(10, 10), padx=(10, 10))
+
+        # multimedia file list
+        self.file_list = Listbox(self.master, height=7)
+        self.file_list.bind('<<ListboxSelect>>', self.on_file_select)
+        self.file_scroll = Scrollbar(self.master, orient=VERTICAL)
+        self.file_list['yscrollcommand'] = self.file_scroll.set
+        self.file_scroll['command'] = self.file_list.yview
+        # place the widget
+        self.file_list.grid(row=1, column=2, rowspan=1, columnspan=2, sticky=N + S + E)
+        self.file_scroll.grid(row=1, column=2, rowspan=1, columnspan=2, sticky=N + S + E)
+        # fill with values
+        for mlt_file in mlt_lib.keys():
+            self.file_list.insert('end', mlt_file)
+
+        # sub-directories list
+        self.dir_list = Listbox(self.master, height=3)
+        self.dir_list.bind('<<ListboxSelect>>', self.on_dir_select)
+        self.dir_scroll = Scrollbar(self.master, orient=VERTICAL)
+        self.dir_list['yscrollcommand'] = self.dir_scroll.set
+        self.dir_scroll['command'] = self.dir_list.yview
+        # place the widget
+        self.dir_list.grid(row=1, column=4, rowspan=1, columnspan=1, sticky=N + S + E)
+        self.dir_scroll.grid(row=1, column=4, rowspan=1, columnspan=1, sticky=N + S + E)
+
+        # buttons upper right
+        self.one_up = Button(self.master, text='one level up', command=one_up)
+        self.one_up.grid(row=0, column=2, sticky=W)
+
+    def rebuild_window(self, master, directory):
+        self.master = master
+        self.refresh_file_list(get_mlt_lib(navigate_to(directory)))
+
+    def refresh_file_list(self, master):
+        self.master = master
+        self.file_list.delete(0, END)
+        print 'a'
+        for mlt_file in mlt_lib.keys():
+            self.file_list.insert('end', mlt_file)
+
+    def on_file_select(self, evt):
+        w = evt.widget
+        index = int(w.curselection()[0])
+        value = w.get(index)
+        print 'You selected item %d: "%s"' % (index, value)
+        self.canvas.delete('all')
+        if 'jpg' in value or 'png' in value or 'gif' in value:
+            if mlt_lib[value]._PhotoImage__size[0] > 200:
+                if mlt_lib[value]._PhotoImage__size[0] > 600:
+                    x = 0 # must resize picture
+                    y = 0
+                else:
+                    x = 0
+                    y = 0
+            else:
+                x = (600 / 2) - (mlt_lib[value]._PhotoImage__size[0] / 2)
+                y = (600 / 2) - (mlt_lib[value]._PhotoImage__size[1] / 2)
+            self.canvas.create_image(x, y, image=mlt_lib[value], anchor="nw")
+        else:
+            self.canvas.create_text(50, 50, text=read_file(mlt_lib[value]))
+            # self.master.update_idletasks()
+
+    def on_dir_select(self, evt):
+        w = evt.widget
+        index = int(w.curselection()[0])
+        value = w.get(index)
+        print 'navigate to %d: "%s"' % (index, value)
+        self.rebuild_window(self, value)
+
+    def on_button_click(self):
+        print 'You clicked button: '
+        # w = evt.widget
+        # refresh_window(w.config('text')[-1])
 
 
 def build_window(directory):
-    global root
     root = Tk()
-
-    # construct encyklopedia
-    global he
-    he = H808E.h808e()
 
     root.title('Hvězdná encyklopedie')
     root.resizable(0, 0)
     root.geometry('900x700')
 
-    # upper menu
-    bar_main = Label(root, text=' Main Menu ...')
-    bar_node = Label(root, text=' show location...')
-    build_categories(he, 410)
-    # implement max image size
-    global canvas
-    canvas = Canvas(root, bd=0, highlightthickness=0, width=700, height=700)
-    # canvas.create_image(0, 0, image=mlt_lib[next(iter(mlt_lib))])
-    canvas.create_line(55, 85, 155, 85, 105, 180, 55, 85)
-    canvas.create_text(20, 30, anchor=W, font="Purisa", text="Most relationships seem so transitory")
-
-    # build directory multimedia list
     global mlt_lib
     mlt_lib = get_mlt_lib(navigate_to(directory))
 
-    # part holding list of multimedia files
-    lb = Listbox(root, height=7)
-    lb.bind('<<ListboxSelect>>', on_select)
-    refresh_list_box(mlt_lib, lb)
-
-    yscroll = Scrollbar(root, orient=VERTICAL)
-    lb['yscrollcommand'] = yscroll.set
-    yscroll['command'] = lb.yview
-
-    # positioning
-    # mlt_img.grid(row=1, column=0, rowspan=3, sticky="wn")
-    canvas.grid(row=1, column=0, rowspan=2, columnspan=2, sticky=N + S + W)
-    lb.grid(row=1, column=2, rowspan=2, columnspan=2, sticky=N + S + E)
-    yscroll.grid(row=1, column=2, rowspan=2, columnspan=2, sticky=N + S + E)
-    bar_main.grid(row=0, column=0)
-    bar_node.grid(row=0, column=1)
-
-    txtng = 'showing picture in list'
-    # button = Button(root, text=txtng, command=get_image)
-    # button.grid(row=1, column=0)
-
+    MainWindow(root)  # .pack(side="top", fill="both", expand=True)
     root.mainloop()
-
-
-def refresh_window(directory):
-    refresh_list_box(get_mlt_lib(navigate_to(directory)), root.lb)
 
 
 def get_mlt_lib(directory):
@@ -68,7 +116,7 @@ def get_mlt_lib(directory):
     mlt_lib = {'filename': PhotoImage(file='*.gif')}
     """
     mlt_lib = {}
-    print 'running over ' + directory
+    # print 'running over ' + directory
     for mlt_file in os.listdir(directory):
         if os.path.isdir(directory + mlt_file):
             continue
@@ -83,26 +131,11 @@ def get_mlt_lib(directory):
     return mlt_lib
 
 
-def on_select(evt):
-    w = evt.widget
-    # print w.curselection()
-    index = int(w.curselection()[0])
-    value = w.get(index)
-    print 'You selected item %d: "%s"' % (index, value)
-    # mlt_img['image'] = mlt_lib[value]
-    canvas.delete('all')
-    if 'jpg' in value or 'png' in value or 'gif' in value:
-        canvas.create_image(0, 0, image=mlt_lib[value], anchor="nw")
-    else:
-        print 'passing file content : ' + read_file(mlt_lib[value])
-        canvas.create_text(20, 20, text=read_file(mlt_lib[value]))
-    # root.update_idletasks()
+def get_sub_dirs(direcotry):
+    sub_dir_lib = {}
+    for sub_dir in directory:
+        sub_dir_lib[sub_dir] = 'aaaa - build full pathh to file'
 
-
-def on_button_click():
-    print 'You clicked button: '
-    #w = evt.widget
-    #refresh_window(w.config('text')[-1])
 
 def fill(image, color):
     """Fill image with a color=(r,b,g)"""
@@ -112,6 +145,22 @@ def fill(image, color):
     hexcode = '#%02x%02x%02x' % (r, b, g)
     horizontal_line = '{' + ' '.join([hexcode] * width) + '}'
     image.put(' '.join([horizontal_line] * width))
+
+
+def one_up():
+    first_file = mlt_lib[next(iter(mlt_lib))]
+    if '\\' in first_file:
+        separator = '\\'
+    elif '/' in first_file:
+        separator = '/'
+
+    # strip filename from path
+    directory = separator.join(first_file.split(separator)[0:-1])
+
+    # go one level up
+    new_directory = separator.join(directory.split(separator)[0:-1])
+    print 'should switch to: ' + new_directory
+    # refresh_window(navigate_to(new_directory))
 
 
 def build_categories(he, parent_node):
@@ -153,18 +202,13 @@ def read_file(filename):
     with open(filename, 'r') as content_file:
         content = content_file.read()
     if 'htm' in filename.split()[-1]:
-        content = ProcessText.htm_to_plain_txt(content)
+        content = TextProcess.htm_to_plain_txt(content)
     return content
+
 
 def get_image():
     """get values that user clicked on"""
     mlt_img['image'] = mlt_lib[lb.get('active')]
-
-
-def refresh_list_box(mlt_lib, lb):
-    lb.delete(0, END)
-    for mlt_file in mlt_lib.keys():
-        lb.insert('end', mlt_file)
 
 
 def navigate_to(directory):
@@ -183,4 +227,3 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     build_window(args.d)
-
