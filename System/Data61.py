187f91213a7d0b6a8093311d172be3855e5c5c48
Kube Kubow <kubow@tiscali.cz>
2017-07-03 16:39:10 +0200

    network cleared

--- System/Data61.py
+++ System/Data61.py
@@ -3,7 +3,7 @@ both algebra and geometry,
 use alone to run simple calculator '''
 
 # Math functions
-class Numbers:
+class Operations:
     '''Basic numeric operations
     define precissness'''
     x = y = 0.00
@@ -27,9 +27,9 @@ class Numbers:
        else:
         return None
         
-    def pwr2(x):
-        """Returns second power of a number"""
-        return x ** 2
+    def pwr(x, y):
+        """Returns y power of a number"""
+        return x ** y
         
     def fact(x):
         """ Return factorial of a number"""
@@ -51,16 +51,18 @@ choice = input("Enter choice(1/2/3/4):")
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
+op = Operations()
+
 if choice == '1':
-   print(num1,"+",num2,"=", add(num1,num2))
+   print(num1,"+",num2,"=", op.add(num1,num2))
 
 elif choice == '2':
-   print(num1,"-",num2,"=", sbt(num1,num2))
+   print(num1,"-",num2,"=", op.sbt(num1,num2))
 
 elif choice == '3':
-   print(num1,"*",num2,"=", mlt(num1,num2))
+   print(num1,"*",num2,"=", op.mlt(num1,num2))
 
 elif choice == '4':
-   print(num1,"/",num2,"=", div(num1,num2))
+   print(num1,"/",num2,"=", op.div(num1,num2))
 else:
    print("Invalid input")
--- System/Network.py
+++ System/Network.py
@@ -1,6 +1,41 @@
 #import argparse
 #import win32com.client
 import os
+from smtplib import SMTP_SSL as SMTP  # secure SMTP (port 465, uses SSL)
+# from smtplib import SMTP            # standard SMTP (port 25, no enc)
+from email.mime.text import MIMEText
+
+class Message():
+    def __init___(self):
+        self.server = 'localhost'
+        self.sender = 'jav@p297c.local'
+        self.reciever = ['jakub.vajda@mdsaptech.cz']
+        # typical values for text_subtype are plain, html, xml
+        self.text_subtype = 'plain'
+
+        # Prepare actual message
+        self.subject = 'Hello!'
+        self.content = '''\
+        The contents of message goes here
+
+        %s
+        ''' % ('some special text goes here')
+
+def send_mail():
+    m = Message()
+
+    message = MIMEText(m.content, m.text_subtype)
+    message['Subject'] = m.subject
+    message['From'] = m.sender
+
+    # Send the message via our own SMTP server
+    conn = SMTP(SMTPserver)
+    xonn.set_debuglevel(False)
+
+    s = smtplib.SMTP(m.server)
+
+    s.sendmail(m.sender, m.reciever, m.message)
+    s.quit()
 
 def monitor_command_output(cmd):
     os.system(cmd)
