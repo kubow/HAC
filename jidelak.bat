0c0fa70c2037a2acb4615557e9c1fce31841523a
kubow <kube.kubow@gmail.com>
2017-08-22 23:21:14 +0200

    runfiles for jidelak

--- jidelak.bat
+++ jidelak.bat
@@ -3,15 +3,11 @@ Set mainHTML=%~dp0index.html
 SET mlt_dir=%~dp0Multimedia\
 SET py_file=%~dp0System\SO74.py
 
-del "%~dp0Multimedia\RestMenu\*.*?"
+del "%mlt_dir%RestMenu\*.*?"
 rem wscript %~dp0System\Reader\ReaderMenu.vbs --not properly set...
 python %py_file% -g restaurant -w %~dp0 -l %mlt_dir%logfile.log
 
 type %~dp0Structure\HTML_Base_Head.txt > %mainHTML%
-
-REM for %%F in (%~dp0Multimedia\RestMenu\*.htm) do (
-REM     echo process file %~dp0Multimedia\RestMenu\%%~nxF
-REM )
 
 echo ^<div id="top"^>^<marquee behavior="scroll" direction="left" scrolldelay="100"^> >> %mainHTML%
 type %~dp0Multimedia\text_horni_lista.txt >> %mainHTML%
@@ -26,6 +22,11 @@ echo ^<script^> >> %mainHTML%
 echo    function loadPages(){ >> %mainHTML%
 echo        var left = "Multimedia/RestMenu/zat.html"; >> %mainHTML%
 echo        var right = "Multimedia/RestMenu/sad.html"; >> %mainHTML%
+
+REM for %%F in (%~dp0Multimedia\RestMenu\*.htm) do (
+REM     echo process file %~dp0Multimedia\RestMenu\%%~nxF
+REM )
+
 echo        document.getElementById('Left').setAttribute('src', left); >> %mainHTML%
 echo        document.getElementById('Right').setAttribute('src', right); >> %mainHTML%
 echo     } >> %mainHTML%
--- jidelak.sh
+++ jidelak.sh
@@ -5,42 +5,30 @@ mainHTML=${PWD}'/index.html'
 mlt_dir=${PWD}'/Multimedia/'
 py_file=${PWD}'/System/SO74.py'
 
+rm ${mlt_dir}RestMenu/*
 python ${py_file} -g restaurant -w ${PWD} -l ${mlt_dir}logfile.log
 
 cat ${PWD}'/Structure/HTML_Base_head.txt' > ${mainHTML}
-echo '<div id="frame"><iframe src="Multimedia/showing1.htm" width="100%%" height="100%%"></iframe></div>' >> ${mainHTML}
+
 echo '<div id="top"><marquee behavior="scroll" direction="left" scrolldelay="100">' >> ${mainHTML}
 cat ${mlt_dir}text_horni_lista.txt >> ${mainHTML}
 echo '</marquee></div>' >> ${mainHTML}
+
+echo '<div id="frame"><iframe id="Left" src="Multimedia/RestMenu/zat.htm" width="49%" height="100%"></iframe>' >> ${mainHTML}
+echo '  <iframe id="Right" src="Multimedia/RestMenu/bla.html" width="49%" height="100%"></iframe></div>' >> ${mainHTML}
+
+echo '<button onclick="loadPages()">Click Me</button>' >> ${mainHTML}
+echo '<script>' >> ${mainHTML}
+echo '   function loadPages(){' >> ${mainHTML}
+echo '       var left = "Multimedia/RestMenu/zat.html";' >> ${mainHTML}
+echo '       var right = "Multimedia/RestMenu/sad.html";' >> ${mainHTML}
+
+echo "       document.getElementById('Left').setAttribute('src', left);" >> ${mainHTML}
+echo "       document.getElementById('Right').setAttribute('src', right);" >> ${mainHTML}
+echo '    }' >> ${mainHTML}
+echo '</script>' >> ${mainHTML}
+
 echo '</body>' >> ${mainHTML}
 echo '</html>' >> ${mainHTML}
 
-#for line in $(sqlite3 ${PWD}/System/DataReaderWeb.db 'select Shortcut,Address,ZomatoAddress,Tag from RestActive'); do
-	#http://stackoverflow.com/questions/10520623/how-to-split-one-string-into-multiple-variables-in-bash-shell
-	#echo $line
-	#IFS='|' read -r shc add zom tag <<< $line
-	#https://www.cyberciti.biz/faq/unix-linux-bash-script-check-if-variable-is-empty/
-	#if [ -z "${add}" ] ; then
-		#echo "daily-menu-conatiner"@${zom}
-	#else
-		#echo ${tag}@${add}
-	#fi
-	#python generate htm's
-	#echo python ${PWD}'/System/DataReaderWeb.py '${shc}
-	#python ${PWD}/System/DataReaderWeb.py ${shc}
-	#echo ¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨¨
-#done
 
-#alternative version using command line
-#http://www.cyberciti.biz/faq/unix-linux-get-the-contents-of-a-webpage-in-a-terminal/
-#http://blog.mattwynne.net/2008/04/26/fetch-and-parse-html-web-page-content-from-bash-wow/
-#page=$(curl http://restaurace-trpaslik.webnode.cz/poledni-menu/)
-#inner=$(xmllint --xpath "//body")
-#echo $page
-#http://stackoverflow.com/questions/21015587/bash-get-content-between-a-pair-of-html-tags
-#xmllint --xpath "//body/node()" f.html
-
-#http://stackoverflow.com/questions/10929453/read-a-file-line-by-line-assigning-the-value-to-a-variable
-#while read p; do
-#  echo $p
-#done <peptides.txt
