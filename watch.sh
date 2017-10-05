76b575393cf8f1fa6faa71a3db5a92d0d7e6279a
kubow <kube.kubow@gmail.com>
2017-10-05 17:21:39 +0200

    watcher updated linux respect

--- watch.sh
+++ watch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #!/usr/bin python
-bbb='/home/kubow/Dokumenty/Script'
-h808e='/home/kubow/Dokumenty/H808E.ctb'
-python ${bbb}/System/H808E.py -d "/home/kubow/Dokumenty/Web/"
-echo "run in ${bbb}"
-#python ${PWD}/System/DirWatch.py -l ${PWD}/Multimedia/logfile.log -w "/home/kubow/Dokumenty/Web/" -d ${PWD}/Multimedia/logfile.sqlite
-python ${bbb}/System/DirWatch.py -l ${bbb}/Multimedia/logfile.log -w "/home/kubow/Dokumenty/Web/" -d ${bbb}/Multimedia/logfile.sqlite
+dir_main=${PWD}
+dir_up=$(dirname $dir_main)
+h808e=$dir_up'/H808E.ctb'
+python ${dir_main}/System/H808E.py -d $dir_up'/Web/'
+echo "run in ${dir_main}"
+python ${dir_main}/System/DirWatch.py -l ${bbb}/Multimedia/logfile.log -w "/home/kubow/Dokumenty/Web/" -d ${bbb}/Multimedia/logfile.sqlite
 echo "done run"
