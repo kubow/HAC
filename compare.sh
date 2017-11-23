a0e26d561d1d6d69bc5111fc3ccfc330bb2d27da
kubow <kube.kubow@gmail.com>
2017-11-24 00:46:43 +0100

    view.sh repaired to be running

--- compare.sh
+++ compare.sh
@@ -1,20 +1,14 @@
 log_file=${PWD}/Multimedia/logfile.log
-if [ "$1" = "b" ]
-then
+if [ "$1" = "b" ]; then
     py_file=${PWD}/System/SO74DB.py
     echo 'python '${py_file}' -m compare -l '$2' -r '$3' -f table_name'
     python ${py_file} -m compare -l $2 -r $3 -f node
-elif [ "$1" = "d" ]
-then
+elif [ "$1" = "d" ]; then
     mlt_dir='/home/kubow/Dokumenty/Web/'
     py_file=${PWD}/System/OS74.py
-    # '-b', help='browse dir', type=str, default='')
-    # '-l', help='list dir', type=str, default='')
-    # '-f', help='file output', type=str, default='')
     echo 'python '${py_file}' -b '${mlt_dir}' -f '${log_file}
     python ${py_file} -b ${mlt_dir} -f ${log_file}
-elif [ "$1" = "t" ]
-then
+elif [ "$1" = "t" ]; then
     echo "text mode"
 else
     echo "1st argument - compare type"
--- view.sh
+++ view.sh
@@ -1,45 +1,39 @@
 log_file=${PWD}/Multimedia/logfile.log
-
-if [ "$1" = "h" ] # HANA
-then
+if [ "$1" = "h" ]; then # HANA
     py_file=hdbalm.py
-    echo 'python '${py_file}
+        echo 'python '${py_file}
     python ${py_file}
-elif [ "$1" = "s" ] # SQLite
-then
+elif [ "$1" = "s" ]; then # SQLite
     py_file=${PWD}/System/SO74DB.py
     echo 'python '${py_file}' -a '$2' -l '${log_file}
     python ${py_file} -a $2 -l ${log_file}
-elif [ "$1" = "t" ] # text file
-then
+elif [ "$1" = "t" ]; then # text file
     py_file=${PWD}/System/SO74TX.py
     python ${py_file} -i $2 -l ${log_file}
-elif [ "$1" = "b" ] # directory
-then
+elif [ "$1" = "b" ]; then # directory
     py_file=${PWD}/System/OS74.py
     echo 'python '${py_file}' -m -m True/False -i '$2' -l '${log_file}
     python ${py_file} -m True -i $2 -l ${log_file}
-elif [ "$1" = "l" ] # list directory files
-then
+elif [ "$1" = "l" ]; then # list directory files
     py_file=${PWD}/System/OS74.py
     echo 'python '${py_file}' -m -m True/False -i '$2' -l '${log_file}
     python ${py_file} -m -i $2 -l ${log_file}
-if [ "$1" = "r" ] # rss
-then
+elif [ "$1" = "r" ]; then # rss
     py_file=${PWD}/System/SO74.py
     echo 'python '${py_file}' -g rss -w '${PWD}' -l '${log_file}
     python ${py_file} -g rss -w ${PWD} -l ${log_file}
 else
     echo '1st parameter:'
-    echo '- DATABASE MODE -'
-    echo 'h - HANA '
-    echo 's - SQLite'
-    echo '- FILE MODE -'
-    echo 't - pure text file'
-    echo '- FOLDER MODE - '
-    echo 'b - browse folders'
-    echo 'l - list folder files'
-    echo '- WEB MODE -'
-    echo 'r - rss feed display'
+    echo '- - - DATABASE MODE - - -'
+    echo '  h - HANA '
+    echo '  s - SQLite'
+    echo '- - - FILE MODE - - -'
+    echo '  t - pure text file'
+    echo '- - - FOLDER MODE - - -'
+    echo '  b - browse folders'
+    echo '  l - list folder files'
+    echo '- - - WEB MODE -'
+    echo '  r - rss feed display'
     echo '2nd parameter: object location'
 fi
+
