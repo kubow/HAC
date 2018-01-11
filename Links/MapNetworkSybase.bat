d63e279f6b06db0bb2b6933922d43fad4bd543a6
Kube Kubow <kubow@tiscali.cz>
2018-01-11 17:18:55 +0100

    added one extra drive

--- Links/MapNetworkSybase.bat
+++ Links/MapNetworkSybase.bat
@@ -12,6 +12,7 @@ net use Q: /delete /yes
 net use O: "\\192.168.6.20\Office" /persistent:yes
 net use T: "\\192.168.6.20\Temp" /persistent:yes
 net use P: "\\192.168.6.20\PS" /persistent:yes
+net use S: "\\192.168.6.20\Documents" /persistent:yes
 net use W: "\\192.168.6.20\Software" /persistent:yes
 net use V: "\\192.168.6.18\sybshare\vmw" /persistent:yes
 net use S: "\\192.168.6.18\sybshare\sybinst" /persistent:yes
