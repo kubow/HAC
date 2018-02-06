5aa6f801a6b5cec6b6aedd4a1c06d98b9b01988a
kubow <kube.kubow@gmail.com>
2018-02-07 00:30:13 +0100

    can run h808e from other locations

--- h808e.sh
+++ h808e.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-media_path=$( realpath ../Web/64/Astrologie/ )
-db_path=$( realpath ../H808E.ctb )
+script_path=`dirname $(readlink -f $0)`
+media_path=$( realpath ${script_path}/../Web/64/Astrologie/ )
+db_path=$( realpath ${script_path}/../H808E.ctb )
 echo "running from "${parent_path}" - watch directory: "${media_path}" - enc database: "${db_path}
 # calling batch menu (platform independent)
-python3 ./System/H808E.py -d ${media_path} -c ${db_path} -l ./Multimedia/logfile.log
+python ${script_path}/System/H808E.py -d ${media_path} -c ${db_path} -l ${script_path}/Multimedia/logfile.log
